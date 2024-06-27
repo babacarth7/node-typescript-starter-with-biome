344fff7350f41192ef58f75037fe8017c71f2263
Babacar Thiam <141820773+babacarth7@users.noreply.github.com>
2024-06-27 09:52:42 +0000

    (stater) initial commit

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+const addition = (a: number, b: number): number => {
+  return a + b;
+};
+
+const x: number = 3;
+const y: number = 10;
+const result: number = addition(x, y);
+
+console.log(`The application name is ${process.env.APP_NAME}`);
+console.log(`The result is ${result}`);
